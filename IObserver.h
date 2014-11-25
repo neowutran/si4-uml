95b71e34513f7cb5d252f263d8cfa0dbe6a04c95
neowutran <me@neowutran.net>
2014-11-25 18:44:19 +0100

    externalisation de IObservable & IObserver

---
+++ IObserver.h
@@ -0,0 +1,28 @@
+#ifndef _IOBSERVABLE_H
+#define _IOBSERVABLE_H
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+class IObservable;
+
+class IObserver{
+
+    private:
+        vector<IObservable*> _observables;
+
+    public:
+        virtual void Update(IObservable* observable, string message) = 0;
+        void Observe(IObservable* observable) {
+            _observables.push_back(observable);
+        }
+
+        void Ignore(IObservable* observable) {
+            _observables.erase(
+                    std::remove(_observables.begin(), _observables.end(), observable),
+                    _observables.end()
+            );
+        }
+};
+
+#endif
--- Robot.h
+++ Robot.h
@@ -9,30 +9,11 @@
 #include "Object.h"
 #include "Position.h"
 #include "State.h"
+#include "IObserver.h"
+#include "IObservable.h"
 using namespace std;
 
 class State;
-class IObservable;
-
-class IObserver{
-
-    private:
-        vector<IObservable*> _observables;
-
-    public:
-        virtual void Update(IObservable* observable, string message) = 0;
-        void Observe(IObservable* observable) {
-            _observables.push_back(observable);
-        }
-
-        void Ignore(IObservable* observable) {
-            _observables.erase(
-                    std::remove(_observables.begin(), _observables.end(), observable),
-                    _observables.end()
-            );
-        }
-};
-
 class IObservable {
 
     private:

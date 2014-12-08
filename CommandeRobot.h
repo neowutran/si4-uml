c2329ab5be1668a7d60b74f63dfae2e33500eef4
neowutran <me@neowutran.net>
2014-12-08 15:02:12 +0100

    correction

--- CommandeRobot.h
+++ CommandeRobot.h
@@ -3,53 +3,53 @@
 
 #include <map>
 #include <string>
-
+#include <vector>
 using namespace std;
-
-class CommandeRobot {
-private:
-    string _name;
-    Invocator _invocator;
-
-public:
-    CommandeRobot(string name) {
-        _name = name;
-        registeredCommands()[name] = this;
-    };
-
-    static CommandeRobot* newCommandeRobot(string type) {
-        return registeredCommands()[type]->virtualConstructor();
-    };
-
-    virtual CommandeRobot* virtualConstructor() = 0;
-
-    string getName() {
-        return _name;
-    }
-
-    void execute(vector<string> parameters) {}
-
-    static map<string, CommandeRobot*>& registeredCommands() {
-        static map<string, CommandeRobot*>* _map = new map<string, CommandeRobot*>;
-        return *_map;
-    }
-
-    friend ostream& operator<<(ostream& os, CommandeRobot& c) {
-        return os << c.getName();
-    }
-
-};
 
 class Invocator {
 
-public:
-    Invocator() { }
+    public:
+        Invocator() { }
 
-    int getInt() {
-
-    }
+        int getInt() {
+            return 0;
+        }
 };
 
 
+
+class CommandeRobot {
+    private:
+        string _name;
+        Invocator _invocator;
+
+    public:
+        CommandeRobot(string name) {
+            _name = name;
+            registeredCommands()[name] = this;
+        };
+
+        static CommandeRobot* newCommandeRobot(string type) {
+            return registeredCommands()[type]->virtualConstructor();
+        };
+
+        virtual CommandeRobot* virtualConstructor() = 0;
+
+        string getName() {
+            return _name;
+        }
+
+        void execute(vector<string> parameters) {}
+
+        static map<string, CommandeRobot*>& registeredCommands() {
+            static map<string, CommandeRobot*>* _map = new map<string, CommandeRobot*>;
+            return *_map;
+        }
+
+        friend ostream& operator<<(ostream& os, CommandeRobot& c) {
+            return os << c.getName();
+        }
+
+};
 
 #endif
